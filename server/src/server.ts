d77fb8cbcab84675db195edfe248daed95676080
Uric <uricbonatti@gmail.com>
2020-06-02 20:00:12 -0300

    Mod 1

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,30 @@
 import express from 'express';
 
-
 const app = express();
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
 
-  response.json(['Diego', 'Cleiton','Robson','Zambabue']);
+app.use(express.json());
+
+const users = ['Diego', 'Cleiton', 'Robson', 'Zambabue'];
+
+app.get('/users', (request, response) => {
+  const search = String(request.query.search);
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+  return response.json(filteredUsers);
+});
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const user = users[id];
+
+  return response.json(user);
+})
+
+app.post('/users', (request, response) => {
+  const data = request.body;
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+  return response.json(user)
 });
 app.listen(3333);
