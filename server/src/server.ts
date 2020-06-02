5573f7c617857994d1a1d08ca118a93cf958e6cf
Uric <uricbonatti@gmail.com>
2020-06-02 15:09:41 -0300

    Preparaçao do Projeto

---
+++ server/src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+
+const app = express();
+app.get('/users', (request, response) => {
+  console.log('Listagem de usuários');
+
+  response.json(['Diego', 'Cleiton','Robson','Zambabue']);
+});
+app.listen(3333);
