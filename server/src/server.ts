c56fc17ba8962459759329f5228c966910e5a143
Uric <uricbonatti@gmail.com>
2020-06-04 13:09:10 -0300

    Frontend v1

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,13 @@
 import express from 'express';
+import routes from './routes';
+import path from 'path';
+import cors from 'cors';
+
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-const users = ['Diego', 'Cleiton', 'Robson', 'Zambabue'];
-
-app.get('/users', (request, response) => {
-  const search = String(request.query.search);
-  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-  return response.json(filteredUsers);
-});
-app.get('/users/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const user = users[id];
-
-  return response.json(user);
-})
-
-app.post('/users', (request, response) => {
-  const data = request.body;
-  const user = {
-    name: data.name,
-    email: data.email,
-  };
-  return response.json(user)
-});
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.listen(3333);
